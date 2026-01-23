50aecbcb54f4b056ec7b97fe9ac86f1cb532738d
Tommaso <tommaso.donolato@itiszuccante.edu.it>
2026-01-23 17:44:06 +0100

    .

--- gioco_STA.py
+++ gioco_STA.py
@@ -4,10 +4,10 @@ import arcade
 WINDOW_WIDHT = 1280
 WINDOW_HEIGHT = 720
 WINDOW_TITLE = "Platformer"
-TILE_SCALING = 0.5
-PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 1
-PLAYER_JUMP_SPEED = 20
+#TILE_SCALING = 0.5
+#PLAYER_MOVEMENT_SPEED = 5
+#GRAVITY = 1
+#PLAYER_JUMP_SPEED = 20
 
 class GameView(arcade.Window):    
     
@@ -16,9 +16,9 @@ class GameView(arcade.Window):
 
         self.background_color = arcade.csscolor.ANTIQUE_WHITE
 
-        #self.player_texture = arcade.load_texture(
-         #   "./game_assets./montanaro.jpg"
-        #)
+        self.player_texture = arcade.load_texture(
+            "./game_assets./sprite_128x128.png"
+        )
 
         self.player_sprite = arcade.Sprite(self.player_texture)
         self.player_sprite.center_x = 64
@@ -27,24 +27,24 @@ class GameView(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.player_list.append(self.player_sprite)
 
-        self.wall_list = arcade.SpriteList(use_spatial_hash = True)
+        #self.wall_list = arcade.SpriteList(use_spatial_hash = True)
 
-        for x in range(0, 1250, 64):
+        #for x in range(0, 1250, 64):
            # wall = arcade.Sprite (sprite muro, scale = TILE_SCALING) 
            #wall.center_x = x
           # wall.center_y = 32
            #self.wall_list.append(wall)
-            pass
-        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+           # pass
+        #coordinate_list = [[512, 96], [256, 96], [768, 96]]
         #mettere lo sprite
-        for coordinate in coordinate_list:
-            wall = arcade.Sprite("sprite muro", scale = TILE_SCALING)
-            wall.position = coordinate
-            self.wall_list.append(wall)
+       # for coordinate in coordinate_list:
+          #  wall = arcade.Sprite("sprite muro", scale = TILE_SCALING)
+           # wall.position = coordinate
+           # self.wall_list.append(wall)
 
-            self.physics_engine = arcade.PhysicsEnginePlatformer(
-                self.player_list, walls = self.wall_list, gravity_constant = GRAVITY
-            )
+           # self.physics_engine = arcade.PhysicsEnginePlatformer(
+          #      self.player_list, walls = self.wall_list, gravity_constant = GRAVITY
+          #  )
 
 
     def setup(self):
@@ -54,30 +54,30 @@ class GameView(arcade.Window):
         self.clear()
 
         self.player_list.draw()
-        self.wall_list.draw()
+       # self.wall_list.draw()
 
-    def on_update(self, delta_time):
-        self.physics_engine.update()
+    #def on_update(self, delta_time):
+       # self.physics_engine.update()
 
-    def on_key_press(self, key ,modifiers):
+    #def on_key_press(self, key ,modifiers):
         
-        if key == arcade.key.UP or key == arcade.key.W:
-            if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+       # if key == arcade.key.UP or key == arcade.key.W:
+           # if self.physics_engine.can_jump():
+              #  self.player_sprite.change_y = PLAYER_JUMP_SPEED
 
        
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        #if key == arcade.key.LEFT or key == arcade.key.A:
+            #self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
 
-        if key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+        #if key == arcade.key.RIGHT or key == arcade.key.D:
+      #      self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
    
-    def on_key_release (self, key, modifiers):
+    #def on_key_release (self, key, modifiers):
         
-        if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.RIGHT or key == arcade.key.D:
-            self.player_sprite.change_x = 0
+       # if key == arcade.key.LEFT or key == arcade.key.A:
+        #    self.player_sprite.change_x = 0
+       # elif key == arcade.key.RIGHT or key == arcade.key.D:
+          #  self.player_sprite.change_x = 0
 
 def main():
     window = GameView()
