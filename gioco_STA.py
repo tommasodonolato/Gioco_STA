51ea1a6a30fd6b72ba03a29a6607fa9e3f4945e3
Tommaso Donolato <tommaso.donolato@itiszuccante.edu.it>
2026-02-05 10:59:59 +0100

    debug

--- gioco_STA.py
+++ gioco_STA.py
@@ -1,25 +1,32 @@
 import arcade
 
 
-WINDOW_WIDHT = 1280
-WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Platformer"
-TILE_SCALING = 0.5
-PLAYER_MOVEMENT_SPEED = 5
-GRAVITY = 1
-PLAYER_JUMP_SPEED = 10
 
-class GameView(arcade.Window):    
-    
+
+class GameView(arcade.Window):   
+
+    WINDOW_WIDTH = 1280
+    WINDOW_HEIGHT = 720
+    WINDOW_TITLE = "Platformer"
+    TILE_SCALING = 1
+    PLAYER_MOVEMENT_SPEED = 5
+    GRAVITY = 0.8
+    PLAYER_JUMP_SPEED = 15
+    BARREL_SCALING = 0.4
+
     def __init__(self):
-        super().__init__(WINDOW_WIDHT, WINDOW_HEIGHT, WINDOW_TITLE)
-
+        super().__init__(GameView.WINDOW_WIDTH, GameView.WINDOW_HEIGHT, GameView.WINDOW_TITLE)
+        self.player_texture = None
+        self.player_sprite = None
+        self.player_list = None
+        self.wall_list = None
         self.background = None
+    def setup(self):
 
         self.player_texture = arcade.load_texture(
             "./game_assets/montanaro.png"
         )
-        self.setup()
+        
         self.player_sprite = arcade.Sprite(self.player_texture)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 128
@@ -28,21 +35,33 @@ class GameView(arcade.Window):
         self.player_list.append(self.player_sprite)
 
         self.wall_list = arcade.SpriteList(use_spatial_hash = True)
-           
+
+        tile_size = 64 * GameView.TILE_SCALING
+
+        for x in range(0, GameView.WINDOW_WIDTH + int(tile_size), int(tile_size)):
+            wall = arcade.Sprite(
+                "./game_assets/terreno.png",
+                scale=GameView.TILE_SCALING
+            )
+            wall.center_x = x
+            wall.center_y = tile_size // 2
+            self.wall_list.append(wall)
+
         coordinate_list = [[512, 96], [256, 96], [768, 96],[64,96],[1024, 96]]
 
-        self.wall_list.append(arcade.Sprite("./game_assets/terreno.png",2,WINDOW_WIDHT/2,10))
-
         for coordinate in coordinate_list:
-            wall = arcade.Sprite("./game_assets/muro_barile.png", scale = TILE_SCALING)
+            wall = arcade.Sprite("./game_assets/muro_barile.png", scale = GameView.BARREL_SCALING)
             wall.position = coordinate
             self.wall_list.append(wall)
 
+       
+
         self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, self.wall_list
+            self.player_sprite, 
+            self.wall_list,
+            gravity_constant=GameView.GRAVITY
         )
 
-    def setup(self):
         
         self.background = arcade.load_texture(
             "./game_assets/sfondo_gioco.png"
@@ -50,8 +69,7 @@ class GameView(arcade.Window):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_texture_rect(self.background,arcade.XYWH(WINDOW_WIDHT/2,WINDOW_HEIGHT/2,WINDOW_WIDHT, WINDOW_HEIGHT))
-        arcade.draw_sprite(self.player_sprite)
+        arcade.draw_texture_rect(self.background,arcade.XYWH(GameView.WINDOW_WIDTH/2, GameView.WINDOW_HEIGHT/2,GameView.WINDOW_WIDTH, GameView.WINDOW_HEIGHT))
         self.player_list.draw()
         self.wall_list.draw()
 
@@ -59,18 +77,19 @@ class GameView(arcade.Window):
         
         self.physics_engine.update()
 
+
     def on_key_press(self, key ,modifiers):
         
         if key == arcade.key.UP or key == arcade.key.W:
             if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                self.player_sprite.change_y = GameView.PLAYER_JUMP_SPEED
 
        
         if key == arcade.key.LEFT or key == arcade.key.A:
-            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+            self.player_sprite.change_x = -GameView.PLAYER_MOVEMENT_SPEED
 
         if key == arcade.key.RIGHT or key == arcade.key.D:
-           self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+           self.player_sprite.change_x = GameView.PLAYER_MOVEMENT_SPEED
    
     def on_key_release (self, key, modifiers):
         
@@ -81,6 +100,7 @@ class GameView(arcade.Window):
 
 def main():
     giochino = GameView()
+    giochino.setup()
     arcade.run()
 
 
