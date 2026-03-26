e90bca4403a1593a7e96709c53207987c23d9711
Tommaso Donolato <tommaso.donolato@itiszuccante.edu.it>
2026-03-26 10:50:45 +0100

    .

--- Game.py
+++ Game.py
@@ -50,7 +50,7 @@ class ParallaxLayer:
 
 class Player(arcade.Sprite):
     def __init__(self):
-        super().__init__("./camminata/image.jpg")
+        super().__init__("./game_assets/montanaro.png")
         self.center_x = 100
         self.center_y = 150
 
